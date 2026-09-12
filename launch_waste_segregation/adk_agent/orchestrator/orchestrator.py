dd685945210347e282368aa3470898c45ab73b78
PriyankaKB <priyankaborgaonkar1@gmail.com>
2026-09-12 18:24:27 +0530

    updated orchestrator

--- launch_waste_segregation/adk_agent/orchestrator/orchestrator.py
+++ launch_waste_segregation/adk_agent/orchestrator/orchestrator.py
@@ -101,7 +101,7 @@ runner = Runner(
 @app.post("/v1/pipeline/sort")
 async def trigger_conveyor_sorting_loop(payload: dict):
     try:
-        session_id = "temp_session"
+        session_id = "temp_session_id"  # You can generate a unique session ID if needed
         user_id = "user_1"
         
         # Create a session using "adk_segregation_app"
